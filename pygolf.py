9729172b62f890a10f4618ed870baeb4ac199103
Gary Martin <gary.martin@physics.org>
2014-06-24 16:40:05 +0100

    now ignores fully commented lines

--- pygolf.py
+++ pygolf.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 import sys
-with open(sys.argv[1]) as f: print len(''.join(f.read().split()))
+print sum(len(l) for l in (''.join(s.split()) for s in open(sys.argv[1]).readlines()) if l[0] != '#')
